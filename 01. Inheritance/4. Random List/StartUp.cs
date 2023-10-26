e78e9341bbf638535aa18acd0247b36327047607
Amxx79 <e.siwanov@gmail.com>
2023-10-27 00:30:33 +0300

    Inheritance

---
+++ 01. Inheritance/4. Random List/StartUp.cs
@@ -0,0 +1,20 @@
+﻿namespace CustomRandomList
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            RandomList list = new RandomList();
+
+            list.Add("1");
+            list.Add("2");
+            list.Add("3");
+            list.Add("4");
+
+            Console.WriteLine(list.RandomString());
+            Console.WriteLine(list.RandomString());
+            Console.WriteLine(list.RandomString());
+            Console.WriteLine(list.RandomString());
+        }
+    }
+}
