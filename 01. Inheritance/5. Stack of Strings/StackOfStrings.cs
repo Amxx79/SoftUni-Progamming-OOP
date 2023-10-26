84575451f771c51f6347688faf400eed39c75c5d
Amxx79 <e.siwanov@gmail.com>
2023-10-27 00:30:48 +0300

    Inheritance

---
+++ 01. Inheritance/5. Stack of Strings/StackOfStrings.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomStack
+{
+    public class StackOfStrings : Stack<string>
+    {
+        public bool IsEmpty()
+        {
+            if (this.Count == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public Stack<string> AddRange(Stack<string> stack)
+        {
+            stack.Push(stack.ToString());
+            return stack;
+        }
+    }
+}
---
+++ 01. Inheritance/5. Stack of Strings/StartUp.cs
@@ -0,0 +1,12 @@
+﻿namespace CustomStack
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            StackOfStrings stackckck = new();
+            stackckck.Push("eee");
+            stackckck.AddRange(stackckck);
+        }
+    }
+}
