993fc4a0693653e8bb7ae1610cd7b13ebe440f5d
Amxx79 <e.siwanov@gmail.com>
2023-10-27 00:30:10 +0300

    Inheritance

---
+++ 01. Inheritance/2. Multiple Inheritance/StartUp.cs
@@ -0,0 +1,14 @@
+﻿namespace Farm
+{
+    public class StartUp
+    {
+        public static void Main()
+        {
+            Dog dog = new Dog();
+            dog.Bark();
+            dog.Eat();
+            Puppy puppy = new Puppy();
+            puppy.Weep();
+        }
+    }
+}
