10672fa2eb936ca5503974b28944ffae0f51d325
Amxx79 <e.siwanov@gmail.com>
2023-10-28 22:59:51 +0300

    Inheritance

---
+++ 01. Inheritance/02. Inheritance - Exercise/NeedForSpeed/StartUp.cs
@@ -0,0 +1,11 @@
+﻿namespace NeedForSpeed
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            RaceMotorcycle raceMotorcycle = new(120, 100);
+            raceMotorcycle.Drive(10);
+        }
+    }
+}
