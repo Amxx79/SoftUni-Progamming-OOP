eda8ecb2e2c58c4d23cc7cfa8d5dec0886f0756f
Amxx79 <e.siwanov@gmail.com>
2023-10-30 18:19:09 +0200

    Inheritance

---
+++ 02. Exercise - Inheritance/Restaurant/StartUp.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Restaurant
+{
+    public class StartUp
+    {
+        public static void Main(string[] args)
+        {
+            //Cake cake = new("Cakkee");
+            //Console.WriteLine($"Name: {cake.Name}");
+            //Console.WriteLine($"Price: {cake.Price}");
+            //Console.WriteLine($"Grams: {cake.Grams}");
+            //Console.WriteLine($"Calories: {cake.Calories}");
+
+            //Fish fish = new("fishhy", 250M);
+            //Console.WriteLine($"Name: {fish.Name}");
+            //Console.WriteLine($"Price: {fish.Price}");
+            //Console.WriteLine($"Grams: {fish.Grams}");
+
+            //Soup soup = new("soup", 999M, 333);
+            //Console.WriteLine($"Name: {soup.Name}");
+            //Console.WriteLine($"Price: {soup.Price}");
+            //Console.WriteLine($"Grams: {soup.Grams}");
+
+            //Coffee coffee = new("Mocca", 30);
+            //Console.WriteLine($"Name: {coffee.Name}");
+            //Console.WriteLine($"Price: {coffee.Price}");
+            //Console.WriteLine($"Mill: {coffee.Milliliters}");
+            //Console.WriteLine($"Caffeine: {coffee.Caffeine}");
+
+            //Tea tea = new("tea", 555M, 550);
+            //Console.WriteLine($"Name: {tea.Name}");
+            //Console.WriteLine($"Price: {tea.Price}");
+            //Console.WriteLine($"Mill: {tea.Milliliters}");
+
+        }
+    }
+}
