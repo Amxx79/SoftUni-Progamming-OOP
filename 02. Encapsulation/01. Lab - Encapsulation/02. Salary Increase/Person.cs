cc84b068a53683877a1b9a45149359ca5809e5ec
Amxx79 <e.siwanov@gmail.com>
2023-10-30 18:23:53 +0200

    Encapsulation

---
+++ 02. Encapsulation/01. Lab - Encapsulation/02. Salary Increase/Person.cs
@@ -0,0 +1,39 @@
+﻿namespace PersonsInfo
+{
+    public class Person
+    {
+        public Person(string firstName, string lastName, int age, decimal salary)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            Salary = salary;
+        }
+
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+        public int Age { get; private set; }
+        public decimal Salary { get; private set; }
+
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName} receives {Salary:F2} leva.";
+        }
+
+        public void IncreaseSalary(decimal percentage)
+        {
+            if (this.Age < 30)
+            {
+                percentage /= 2;
+                this.Salary += Salary * percentage / 100;
+            }
+            else
+            {
+                this.Salary += Salary * percentage / 100;
+            }
+        }
+    }
+
+
+}
---
+++ 02. Encapsulation/01. Lab - Encapsulation/02. Salary Increase/StartUp.cs
@@ -0,0 +1,31 @@
+﻿namespace PersonsInfo
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<Person> list = new List<Person>();
+
+            for (int i = 0; i < n; i++) 
+            {
+                string[] personInfo = Console.ReadLine().Split();
+                string fN = personInfo[0];
+                string lN = personInfo[1];
+                int age = int.Parse(personInfo[2]);
+                decimal sl = decimal.Parse(personInfo[3]);
+
+                Person person = new Person(fN, lN, age, sl);
+                list.Add(person);
+            }
+            int percentage = int.Parse(Console.ReadLine());
+
+            foreach (Person person in list)
+            {
+                person.IncreaseSalary(percentage);
+                Console.WriteLine(person);
+            }
+        }
+
+    }
+}
