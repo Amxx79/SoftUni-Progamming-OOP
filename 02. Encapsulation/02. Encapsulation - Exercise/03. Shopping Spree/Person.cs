9efd5ec1bde266fce9e6df7efae72e62ff1c46a1
Amxx79 <e.siwanov@gmail.com>
2023-11-02 02:15:04 +0200

    Encapsulation

---
+++ 02. Encapsulation/02. Encapsulation - Exercise/03. Shopping Spree/Person.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _03._Shopping_Spree
+{
+    public class Person
+    {
+        private string name;
+        private decimal money;
+
+        public Person(string name, decimal money)
+        {
+            this.Name = name;
+            this.Money = money;
+            this.Bag = new List<Product>();
+        }
+
+        public string Name
+        {
+            get => name;
+
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentNullException("Name cannot be empty");
+                }
+                name = value;
+            }
+        }
+        public decimal Money
+        {
+            get => money;
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Money cannot be negative");
+                }
+                money = value;
+            }
+        }
+
+        private List<Product> Bag { get; set; }
+
+        public void AddProduct(Product product)
+        {
+            Bag.Add(product);
+        }
+
+
+        public override string ToString()
+        {
+            string productsString = Bag.Any()
+                 ? string.Join(", ", Bag.Select(p => p.Name))
+                 : "Nothing bought";
+
+            return $"{Name} - {productsString}";
+        }
+    }
+}
---
+++ 02. Encapsulation/02. Encapsulation - Exercise/03. Shopping Spree/StartUp.cs
@@ -0,0 +1,62 @@
+﻿namespace _03._Shopping_Spree
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            string[] people = Console.ReadLine().Split(";", StringSplitOptions.RemoveEmptyEntries);
+            string[] products = Console.ReadLine().Split(";", StringSplitOptions.RemoveEmptyEntries);
+
+            List<Person> personsList = new List<Person>();
+            List<Product> productsList = new List<Product>();
+            try
+            {
+                foreach (var p in people)
+                {
+                    string[] personData = p.Split("=", StringSplitOptions.RemoveEmptyEntries);
+                    Person person = new(personData[0], decimal.Parse(personData[1]));
+                    personsList.Add(person);
+                }
+
+                foreach (var food in products)
+                {
+                    string[] foodData = food.Split("=", StringSplitOptions.RemoveEmptyEntries);
+                    Product product = new(foodData[0], decimal.Parse(foodData[1]));
+                    productsList.Add(product);
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Environment.Exit(0);
+            }
+
+            string command;
+            while ((command = Console.ReadLine()) != "END")
+            {
+
+                string[] person = command.Split();
+                string name = person[0];
+                string product = person[1];
+
+                Person currentPerson = personsList.FirstOrDefault(p => p.Name == name);
+                Product currentProduct = productsList.FirstOrDefault(p => p.Name == product);
+
+                if (currentPerson.Money >= currentProduct.Cost)
+                {
+                    Console.WriteLine($"{currentPerson.Name} bought {currentProduct.Name}");
+                    currentPerson.Money -= currentProduct.Cost;
+                    currentPerson.AddProduct(currentProduct);
+                }
+                else if (currentPerson.Money < currentProduct.Cost)
+                {
+                    Console.WriteLine($"{currentPerson.Name} can't afford {currentProduct.Name}");
+                }
+            }
+
+            
+
+            Console.WriteLine(string.Join(Environment.NewLine, personsList));
+        }
+    }
+}
