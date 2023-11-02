7018b7eab2129491a4ad2fffbba6373cfd4e4737
Amxx79 <e.siwanov@gmail.com>
2023-11-02 02:15:34 +0200

    Encapsulation

---
+++ 02. Encapsulation/02. Encapsulation - Exercise/01. Class Box Data/Models/Box.cs
@@ -0,0 +1,66 @@
+﻿    public class Box
+    {
+        private double length;
+        private double width;
+        private double height;
+
+
+        public Box(double length, double width, double height)
+        {
+            Length = length;
+            Widht = width;
+            Heigth = height;
+        }
+
+        public double Length
+        {
+            get
+            {
+                return length;
+            }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Length cannot be zero or negative.");
+                }
+                length = value;
+            }
+        }
+
+        public double Widht
+        {
+            get
+            {
+                return width;
+            }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Width cannot be zero or negative.");
+                }
+                width = value;
+            }
+        }
+
+        public double Heigth
+        {
+            get
+            {
+                return height;
+            }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Heigth cannot be zero or negative.");
+                }
+                height = value;
+            }
+        }
+
+        public double SurfaceArea() => (2 * Length * Widht) + (2 * Length * Heigth) + (2 * Widht * Heigth);
+        public double LateralSurfaceArea() => (2 * Length * Heigth) + (2 * Widht * Heigth);
+        public double Volume() => Length * Widht * Heigth;
+    }
---
+++ 02. Encapsulation/02. Encapsulation - Exercise/01. Class Box Data/Program.cs
@@ -0,0 +1,16 @@
+﻿
+    double Length = double.Parse(Console.ReadLine());
+    double Width = double.Parse(Console.ReadLine());
+    double Height = double.Parse(Console.ReadLine());
+try
+{
+    Box box = new(Length, Width, Height);
+
+    Console.WriteLine($"Surface Area - {box.SurfaceArea():F2}");
+    Console.WriteLine($"Lateral Surface Area - {box.LateralSurfaceArea():F2}");
+    Console.WriteLine($"Volume - {box.Volume():F2}");
+}
+catch(Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
