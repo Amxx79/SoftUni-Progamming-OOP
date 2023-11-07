d6eeb6a5f0d4b155fa29e890bf61e0a7202b5797
Amxx79 <e.siwanov@gmail.com>
2023-11-07 21:58:20 +0200

    Interfaces and Abstraction

---
+++ 03. Interfaces and Abstraction/02. Interfaces and Abstraction - Exercise/04. Border Control/Models/Human.cs
@@ -0,0 +1,16 @@
+﻿namespace BorderControl.Models.Interfaces
+{
+    public class Human : IHumanable
+    {
+        public Human(string name, int age, string id)
+        {
+            Name = name;
+            Id = id;
+            Age = age;
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Id { get; set; }
+    }
+}
---
+++ 03. Interfaces and Abstraction/02. Interfaces and Abstraction - Exercise/04. Border Control/StartUp.cs
@@ -0,0 +1,36 @@
+﻿using BorderControl.Models.Interfaces;
+
+
+string command;
+List<string> enteringThings = new List<string>();
+
+while ((command = Console.ReadLine()) != "End")
+{
+    string[] infoData = command.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+    string name = infoData[0];
+
+    if (infoData.Length == 3)
+    {
+        int age = int.Parse(infoData[1]);
+        string id = infoData[2];
+        Human human = new(name, age, id);
+        enteringThings.Add(human.Id);
+    }
+    else if (infoData.Length == 2)
+    {
+        string id = infoData[1];
+        Robot robot = new(name, id);
+        enteringThings.Add(robot.Id);
+    }
+}
+
+string fakeEndIds = Console.ReadLine();
+List<string> allFakeIds = new();
+foreach (var id in enteringThings)
+{
+    if (id.EndsWith(fakeEndIds))
+    {
+        Console.WriteLine(id);
+    }
+}
