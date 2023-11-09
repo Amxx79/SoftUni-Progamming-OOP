02f3aa02891ff92d0ea552d410fc69fd516cd6fd
Amxx79 <e.siwanov@gmail.com>
2023-11-09 09:57:40 +0200

    Polymorphism

---
+++ 04. Polymorphism/01. Polymorphism - Lab/1. MathOperation/MathOperations.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Operations
+{
+    public class MathOperations
+    {
+        public int Add(int n1, int n2)
+        {
+            return n1 + n2;
+        }
+        public double Add(double num1, double num2, double num3)
+        {
+            return num1 + num2 + num3;
+        }
+        public decimal Add(decimal num1, decimal num2, decimal num3)
+        {
+            return num1 + num2 + num3;
+
+        }
+
+    }
+}
