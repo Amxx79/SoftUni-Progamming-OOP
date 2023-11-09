caf510eaefb33c0830214b63fc59d8e630b1b954
Amxx79 <e.siwanov@gmail.com>
2023-11-09 09:53:34 +0200

    Interfaces and Abtstraction

---
+++ 03. Interfaces and Abstraction/02. Interfaces and Abstraction - Exercise/05. BirthdayCelebrations/Models/Human.cs
@@ -0,0 +1,21 @@
+﻿using BirthdayCelebrations.Models.Interfaces;
+
+namespace BorderControl.Models.Interfaces
+{
+    public class Human : IHumanable
+    {
+        public Human(string name, int age, string id, string[] birthday)
+        {
+            Name = name;
+            Id = id;
+            Age = age;
+            BirthDay = birthday;
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Id { get; set; }
+        public string[] BirthDay { get; set; }
+
+    }
+}
---
+++ 03. Interfaces and Abstraction/02. Interfaces and Abstraction - Exercise/05. BirthdayCelebrations/StartUp.cs
@@ -0,0 +1,46 @@
+﻿using BirthdayCelebrations.Models;
+using BorderControl.Models.Interfaces;
+using System.Text;
+
+string command;
+List<string[]> years = new();
+
+while ((command = Console.ReadLine()) != "End")
+{
+    string[] infoData = command.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+    string type = infoData[0];
+    string name = infoData[1];
+
+    if (type == "Citizen")
+    {
+        int age = int.Parse(infoData[2]);
+        string id = infoData[3];
+        string[] birthday = infoData[4].Split("/", StringSplitOptions.RemoveEmptyEntries);
+        Human human = new(name, age, id, birthday);
+        years.Add(birthday);
+    }
+    else if (type == "Pet")
+    {
+        string[] birthday = infoData[2].Split("/", StringSplitOptions.RemoveEmptyEntries);
+        Pet pet = new(name, birthday);
+        years.Add(birthday);
+    }
+    else if (type == "Robot")
+    {
+        string id = infoData[2];
+        Robot robot = new(name, id);
+    }
+}
+string yearToCompare = Console.ReadLine();
+
+foreach (var year in years)
+{
+    string currentYear = year[2];
+    if (currentYear == yearToCompare)
+    {
+        Console.Write(year[0] + "/");
+        Console.Write(year[1] + "/");
+        Console.Write(year[2]);
+        Console.WriteLine();
+    }
+}
