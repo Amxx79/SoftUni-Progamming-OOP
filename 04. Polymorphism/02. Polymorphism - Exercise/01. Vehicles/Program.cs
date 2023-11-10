ac4f1187e6fd747d3bbaf59565de50d73538efb9
Amxx79 <e.siwanov@gmail.com>
2023-11-10 20:17:24 +0200

    Polymorphism

---
+++ 04. Polymorphism/02. Polymorphism - Exercise/01. Vehicles/Program.cs
@@ -0,0 +1,12 @@
+﻿using Vehicle.Core.Interfaces;
+using Vehicle.IO.Interfaces;
+using Vehicle.IO;
+using Vehicle.Core;
+using Vehicle.Factories;
+
+IReader reader = new ConsoleReader();
+IWriter writer = new ConsoleWriter();
+
+IEngine engine = new Engine(reader, writer);
+
+engine.Run();
