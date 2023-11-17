eb831e94e552a7e868a524b2b28e4d07d16a5677
Amxx79 <e.siwanov@gmail.com>
2023-11-18 00:51:46 +0200

    SOLID

---
+++ 05. SOLID/01. SOLID - Lab/01. Stream Progress Info/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace P01.Stream_Progress
+{
+    public class Program
+    {
+        static void Main()
+        {
+            //Music music = new("john", "this", 23, 3);
+
+            Music song = new Music("john", "this", 23, 3);
+
+            StreamProgressInfo stream = new StreamProgressInfo(song);
+            Console.WriteLine(stream.CalculateCurrentPercent());
+        }
+    }
+}
