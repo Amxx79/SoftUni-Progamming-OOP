840d06973df51b05e58e5e09c94f40df8086b3a6
Amxx79 <e.siwanov@gmail.com>
2023-11-18 00:52:02 +0200

    SOLID

---
+++ 05. SOLID/01. SOLID - Lab/02. Graphic Editor/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace P02.Graphic_Editor
+{
+    class Program
+    {
+        static void Main()
+        {
+            IShape form = new Circle();
+            GraphicEditor editor = new GraphicEditor(form);
+            editor.DrawShape();
+            IShape rect = new Rectangle();
+            GraphicEditor rectangle = new(rect);
+            rectangle.DrawShape();
+        }
+    }
+}
