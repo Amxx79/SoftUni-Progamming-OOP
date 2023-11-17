378446ef336605c1950a1859db19d37fba7cd67f
Amxx79 <e.siwanov@gmail.com>
2023-11-18 00:52:13 +0200

    SOLID

---
+++ 05. SOLID/01. SOLID - Lab/03. Detail Printer/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace P03.DetailPrinter
+{
+    class Program
+    {
+        static void Main()
+        {
+            //Input Info
+            Employee employee = new Employee("John The Employee");
+            ICollection<string> documents = new List<string>()
+            {
+            "Document One",
+            "Document Two",
+            "Document Three"
+            };
+            Employee manager = new Manager("Peter The Manager", documents);
+
+            //Action
+            List<Employee> employees = new List<Employee>();
+            employees.Add(employee);
+            employees.Add(manager);
+
+            DetailsPrinter printer = new DetailsPrinter(employees);
+            printer.PrintDetails();
+        }
+    }
+}
