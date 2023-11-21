74fe362a9d438d58cb6e4b780447f33c3bae4643
Amxx79 <e.siwanov@gmail.com>
2023-11-21 22:50:02 +0200

    Reflection and Attributes

---
+++ 06. Reflection and Attributes/01. Reflection and Attributes - Lab/01. Stealer/Spy.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Channels;
+using System.Threading.Tasks;
+
+namespace Stealer
+{
+    public class Spy
+    {
+        public string StealFieldInfo(string name, params string[] nameOfFields)
+        {
+            Type classType = Type.GetType(name);
+            FieldInfo[] classFields = classType.GetFields((BindingFlags)60);
+
+            StringBuilder sb = new();
+
+            Object classInstance = Activator.CreateInstance(classType, new object[] { });
+
+            sb.AppendLine($"Class under investigation: {name}");
+
+            foreach (FieldInfo field in classFields.Where(f => nameOfFields.Contains(f.Name)))
+            {
+                sb.AppendLine($"{field.Name} = {field.GetValue(classInstance)}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+    }
+}
