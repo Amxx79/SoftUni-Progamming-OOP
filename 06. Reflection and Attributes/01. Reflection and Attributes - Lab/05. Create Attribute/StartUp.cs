f903ea6e3ae3115271cf26564cb8b9de6f6f33e6
Amxx79 <e.siwanov@gmail.com>
2023-11-21 22:49:22 +0200

    Reflection and Attributes

---
+++ 06. Reflection and Attributes/01. Reflection and Attributes - Lab/05. Create Attribute/StartUp.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace AuthorProblem
+{
+    [Author("Victor")]
+    public class StartUp
+    {
+        [Author("George")]
+        static void Main(string[] args)
+        {
+
+        }
+    }
+    [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true)]
+    class AuthorAttribute : Attribute
+    {
+        public AuthorAttribute(string name)
+        {
+            this.Name = name;
+        }
+        public string Name { get; set; }
+    }
+}
