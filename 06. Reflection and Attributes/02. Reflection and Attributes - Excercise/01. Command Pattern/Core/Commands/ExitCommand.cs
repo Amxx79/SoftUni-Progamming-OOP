4646b0227edb13502736bd2ac5dc65b24de105ae
Amxx79 <e.siwanov@gmail.com>
2023-11-22 16:06:07 +0200

    Reflection and Attributes

---
+++ 06. Reflection and Attributes/02. Reflection and Attributes - Excercise/01. Command Pattern/Core/Commands/ExitCommand.cs
@@ -0,0 +1,15 @@
+﻿using CommandPattern.Core.Contracts;
+using System;
+using System.Collections.Generic;
+namespace CommandPattern.Core.Commands
+{
+    public class ExitCommand : ICommand
+    {
+        public string Execute(string[] args)
+        {
+            Environment.Exit(0);
+
+            return null;
+        }
+    }
+}
