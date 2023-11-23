4226d014fbc5f18a264fc4b024a49742649947fd
Amxx79 <e.siwanov@gmail.com>
2023-11-23 02:59:35 +0200

    Reflection and Attributes

---
+++ 06. Reflection and Attributes/02. Reflection and Attributes - Excercise/02. Validation Attribute/Utils/Validator.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using ValidationAttributes.Attributes;
+
+namespace ValidationAttributes.Utils
+{
+    public class Validator
+    {
+        public static bool IsValid(object obj)
+        {
+            Type objType = obj.GetType();
+            var a = objType.GetProperties();
+
+            PropertyInfo[] propertyInfo = objType
+                .GetProperties()
+                .Where(p => p.CustomAttributes
+                .Any(ca => typeof(MyValidationAttribute)
+                .IsAssignableFrom(ca.AttributeType))).ToArray();
+
+            foreach ( PropertyInfo propInfo in propertyInfo) 
+            {
+                IEnumerable<MyValidationAttribute> attributes = propInfo
+                    .GetCustomAttributes()
+                    .Where(ca => typeof(MyValidationAttribute)
+                    .IsAssignableFrom(ca.GetType()))
+                    .Cast<MyValidationAttribute>();
+                foreach (MyValidationAttribute attr in attributes)
+                {
+                    if (!attr.IsValid(propInfo.GetValue(obj)))
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
