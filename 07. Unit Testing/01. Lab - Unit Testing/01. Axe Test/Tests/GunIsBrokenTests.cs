94419e070618940efaeae74c23a2a1c66d8e1b80
Amxx79 <e.siwanov@gmail.com>
2023-11-24 02:47:24 +0200

    Unit Testing

---
+++ 07. Unit Testing/01. Lab - Unit Testing/01. Axe Test/Tests/GunIsBrokenTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skeleton.Tests
+{
+    [TestFixture]
+    public class GunIsBrokenTests
+    {
+        [Test]
+        public void AxeDurability_IsUnderZero_GunIsBroken()
+        {
+            Axe axe = new Axe(10, 0);
+            Dummy dummy = new Dummy(10, 10);
+
+            Assert.Throws<InvalidOperationException>(() => axe.Attack(dummy));
+        }
+    }
+}
