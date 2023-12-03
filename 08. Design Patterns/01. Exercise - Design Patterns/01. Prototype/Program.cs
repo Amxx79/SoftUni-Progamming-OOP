1fa8d985c24c904a23c5bee19b260a4cb60aba12
Amxx79 <e.siwanov@gmail.com>
2023-12-03 02:09:47 +0200

    Design Patterns

---
+++ 08. Design Patterns/01. Exercise - Design Patterns/01. Prototype/Program.cs
@@ -0,0 +1,15 @@
+﻿using _01._Prototype;
+
+SandwichMenu sandwichMenu = new SandwichMenu();
+
+sandwichMenu["BLT"] = new Sandwich("Wheat", "Bacon", "", "Lettuce, Tomato");
+sandwichMenu["PB&J"] = new Sandwich("White", "", "", "Peanut, Butter, Jelly");
+sandwichMenu["Turkey"] = new Sandwich("Rye", "Turkey", "Swiss", "Lettuce, Onion, Tomato");
+
+sandwichMenu["LoadedBLT"] = new Sandwich("Wheat", "Turkey, Bacon", "American", "Lettuce, Tomato, Onion, Olives");
+sandwichMenu["ThreeMeatCombo"] = new Sandwich("Rye", "Turkey, Ham, Salami", "Provolone", "Lettuce, Onion");
+sandwichMenu["Vegeterian"] = new Sandwich("Wheat", "", "", "Lettuce, Onion, Tomato, Olives, Spinach");
+
+Sandwich sandich1 = sandwichMenu["BLT"].Clone() as Sandwich;
+Sandwich sandich2 = sandwichMenu["ThreeMeatCombo"].Clone() as Sandwich;
+Sandwich sandich3 = sandwichMenu["Vegeterian"].Clone() as Sandwich;
