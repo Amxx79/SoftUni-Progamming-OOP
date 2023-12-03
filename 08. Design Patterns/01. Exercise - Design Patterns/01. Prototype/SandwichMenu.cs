e1de6a2c5701d5aabe165e7b4dae5f179a199bee
Amxx79 <e.siwanov@gmail.com>
2023-12-03 02:09:25 +0200

    Design Patterns

---
+++ 08. Design Patterns/01. Exercise - Design Patterns/01. Prototype/SandwichMenu.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01._Prototype
+{
+    public class SandwichMenu
+    {
+        private IDictionary<string, SandwichPrototype> sandwiches;
+
+        public SandwichMenu()
+        {
+            sandwiches = new Dictionary<string, SandwichPrototype>();
+        }
+
+
+        public SandwichPrototype this[string name]
+        {
+            get => sandwiches[name];
+            set => sandwiches.Add(name, value);
+        }
+    }
+}
