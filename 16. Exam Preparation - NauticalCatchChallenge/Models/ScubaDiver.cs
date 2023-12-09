28787478570cbc7c592f35e249b2f5fd0073b9e8
Amxx79 <e.siwanov@gmail.com>
2023-12-10 00:21:47 +0200

    Exam Preparation

---
+++ 16. Exam Preparation - NauticalCatchChallenge/Models/ScubaDiver.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NauticalCatchChallenge.Models
+{
+    public class ScubaDiver : Diver
+    {
+        private const int oxygenLevel = 540;
+        private const double oxyDecreaseIndex = 0.3;
+
+        public ScubaDiver(string name)
+            : base(name, oxygenLevel)
+        {
+
+        }
+
+        public override void Miss(int timeToCatch)
+        {
+            int usedOxy = (int)Math.Round(timeToCatch * oxyDecreaseIndex);
+            base.OxygenLevel -= usedOxy;
+        }
+
+        public override void RenewOxy()
+        {
+            base.OxygenLevel = oxygenLevel;
+        }
+    }
+}
