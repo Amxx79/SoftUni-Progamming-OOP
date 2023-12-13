e35c246a82f7dec489543fe750ead7b4c17162b3
Amxx79 <e.siwanov@gmail.com>
2023-12-14 01:07:41 +0200

    Exam Preparation

---
+++ 09. Exam Preparation/11. Exam Preparation - RobotService/Models/Robot.cs
@@ -0,0 +1,108 @@
+﻿using RobotService.Models.Contracts;
+using RobotService.Utilities.Messages;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotService.Models
+{
+    public abstract class Robot : IRobot
+    {
+        private string model;
+        private int batteryCapacity;
+        List<int> interfaceStandards;
+        public Robot(string model, int batteryCapacity, int conversionCapacityIndex)
+        {
+            Model = model;
+            BatteryCapacity = batteryCapacity;
+            ConvertionCapacityIndex = conversionCapacityIndex;
+            BatteryLevel = BatteryCapacity;
+            interfaceStandards = new List<int>();
+        }
+        public string Model
+        {
+            get => model;
+            private set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                {
+                    throw new ArgumentException(ExceptionMessages.ModelNullOrWhitespace);
+                }
+                model = value;
+            }
+        }
+
+        public int BatteryCapacity
+        {
+            get => batteryCapacity;
+            private set
+            {
+                if (batteryCapacity < 0)
+                {
+                    throw new ArgumentException(ExceptionMessages.BatteryCapacityBelowZero);
+                }
+                batteryCapacity = value;
+            }
+        }
+
+        public int BatteryLevel { get; private set; }
+
+        public int ConvertionCapacityIndex { get; private set; }
+
+        public IReadOnlyCollection<int> InterfaceStandards => interfaceStandards;
+
+        public void Eating(int minutes)
+        {
+            int energy = minutes * ConvertionCapacityIndex;
+            if (BatteryLevel >= BatteryCapacity)
+            {
+                BatteryLevel = BatteryCapacity;
+            }
+            else
+            {
+                BatteryLevel += energy;
+            }
+        }
+
+        public bool ExecuteService(int consumedEnergy)
+        {
+            if (BatteryLevel >= consumedEnergy)
+            {
+                BatteryLevel -= consumedEnergy;
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void InstallSupplement(ISupplement supplement)
+        {
+            BatteryCapacity -= supplement.BatteryUsage;
+            BatteryLevel -= supplement.BatteryUsage;
+            interfaceStandards.Add(supplement.InterfaceStandard);
+        }
+        public override string ToString()
+        {
+            StringBuilder sb = new();
+            sb.AppendLine($"{this.GetType().Name} {Model}:");
+            sb.AppendLine($"--Maximum battery capacity: {BatteryCapacity}");
+            sb.AppendLine($"--Current battery level: {BatteryLevel}");
+            sb.Append("--Supplements installed: ");
+            if (interfaceStandards.Any())
+            {
+                sb.AppendLine(string.Join(' ', InterfaceStandards));
+            }
+            else
+            {
+                sb.AppendLine("none");
+
+            }
+            return sb.ToString().Trim();
+        }
+    }
+}
---
+++ 09. Exam Preparation/11. Exam Preparation - RobotService/Models/SpecializedArm.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotService.Models
+{
+    public class SpecializedArm : Supplement
+    {
+        private const int interfaceStandart = 10_045;
+        private const int batteryUsage = 10_000;
+        public SpecializedArm() 
+            : base(interfaceStandart, batteryUsage)
+        {
+        }
+    }
+}
