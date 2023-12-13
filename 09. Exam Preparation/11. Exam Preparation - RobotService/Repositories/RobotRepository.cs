e1213cb25feffa0fdc45894c8191f27ffcc751fd
Amxx79 <e.siwanov@gmail.com>
2023-12-14 01:07:31 +0200

    Exam Preparation

---
+++ 09. Exam Preparation/11. Exam Preparation - RobotService/Repositories/RobotRepository.cs
@@ -0,0 +1,44 @@
+﻿using RobotService.Models.Contracts;
+using RobotService.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotService.Repositories
+{
+    public class RobotRepository : IRepository<IRobot>
+    {
+        private List<IRobot> robots;
+        public RobotRepository()
+        {
+            robots = new List<IRobot>();
+        }
+        public void AddNew(IRobot model)
+        {
+            robots.Add(model);
+        }
+
+        public IRobot FindByStandard(int interfaceStandard)
+        {
+            IRobot robot = robots.FirstOrDefault(r => r.InterfaceStandards.Contains(interfaceStandard));
+            if (robot != null)
+            {
+                return robot;
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public IReadOnlyCollection<IRobot> Models() => robots;
+
+        public bool RemoveByName(string robotModel)
+        {
+            IRobot robot = robots.FirstOrDefault(r => r.Model == robotModel);
+            return robots.Remove(robot);
+        }
+    }
+}
---
+++ 09. Exam Preparation/11. Exam Preparation - RobotService/Repositories/SupplementRepository.cs
@@ -0,0 +1,35 @@
+﻿using RobotService.Models.Contracts;
+using RobotService.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RobotService.Repositories
+{
+    public class SupplementRepository : IRepository<ISupplement>
+    {
+        private List<ISupplement> models;
+        public SupplementRepository()
+        {
+            models = new List<ISupplement>();
+        }
+        public void AddNew(ISupplement model)
+        {
+            models.Add(model);
+        }
+
+        public ISupplement FindByStandard(int interfaceStandard)
+        {
+            return models.FirstOrDefault(s => s.InterfaceStandard == interfaceStandard);
+        }
+
+        public IReadOnlyCollection<ISupplement> Models() => models.AsReadOnly();
+
+        public bool RemoveByName(string typeName)
+        {
+            return models.Remove(models.FirstOrDefault(s => nameof(s) ==  typeName));
+        }
+    }
+}
