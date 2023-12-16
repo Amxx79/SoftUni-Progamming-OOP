4c47b4072bd3072f30e48ea8bdc88b298f52198a
Amxx79 <e.siwanov@gmail.com>
2023-12-17 00:52:55 +0200

    Exam Preparation

---
+++ 09. Exam Preparation/15. Exam Preparation - BookingApp/Models/Rooms/Room.cs
@@ -0,0 +1,38 @@
+﻿using BookingApp.Models.Rooms.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookingApp.Models.Rooms
+{
+    public abstract class Room : IRoom
+    {
+        private double pricePerNeight;
+        public Room(int bedCapacity)
+        {
+            BedCapacity = bedCapacity;
+            PricePerNight = 0;
+        }
+        public int BedCapacity { get; private set; }
+
+        public double PricePerNight
+        {
+            get => pricePerNeight;
+            private set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Price cannot be negative!");
+                }
+                pricePerNeight = value;
+            }
+        }
+
+        public void SetPrice(double price)
+        {
+            PricePerNight = price;
+        }
+    }
+}
